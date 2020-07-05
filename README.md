83cc537c11f9ab097062677b8d30e77bd4f424b1
marc <marc_smith@gmx.com>
2020-07-05 11:58:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# watchman - port ping module
+
+1. configure mutt in .muttrc
+2. copy .muttrc to /root/.muttrc
+3. run ./watchman_ping.sh [-i 192.168.0.20] [-n myhost] [-s interval] [-m mail@fqdn.com]
---
+++ watchman_ping.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-i 192.168.0.20] [-n myhost] [-s interval] [-m mail@fqdn.com]" 1>&2; exit 1; }
+
+while getopts ":i:n:s:m:" o; do
+    case "${o}" in
+        i)
+            i=${OPTARG}
+            ;;
+        n)
+            n=${OPTARG}
+            ;;
+        s)                                                                                                                                                     
+            s=${OPTARG}                                                                                                                                        
+            ;;
+        m)                                                                                                                                                     
+            m=${OPTARG}                                                                                                                                        
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${i}" ] || [ -z "${n}" ] || [ -z "${s}" ] || [ -z "${m}" ]; then
+    usage
+fi
+
+#echo $i " " $n " " $s " " $m " "
+
+while [ true ]; do
+  sleep $s
+  nc -w 4 -vz $i 22 > /dev/null
+  if [ $? -eq 1 ]; then
+    printf "`date`: $n IP: $i is DOWN.\n" >> ./watchman_ping_history.log                                                                           
+      if test -f ./$n"_down"; then
+        echo "not sending another mail."
+      else
+        echo `date` > mail
+        echo "$i is DOWN!" | mutt -s "$i is DOWN!" $m
+        touch ./$n"_down"
+      fi                                                                                                          
+    else
+      if test -f ./$n"_down"; then
+        rm ./$n"_down"                                                                                                                             
+        printf "`date`: $n with IP: $i is UP AGAIN.\n" >> ./watchman_ping_history.log
+        echo "$i is DOWN!" | mutt -s "$i is UP AGAIN!" $m
+      else                                                                                                                              
+      printf "`date`: $n with IP: $i is UP.\n" >> ./watchman_ping_history.log                                                                             
+      fi
+  fi
+done
