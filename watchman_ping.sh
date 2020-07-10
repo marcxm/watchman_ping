42f7ce7431dd270ba302a1878492203ecd2b6269
marc <marc_smith@gmx.com>
2020-07-10 22:06:28 +0200

    various fixes

--- watchman_ping.sh
+++ watchman_ping.sh
@@ -3,6 +3,8 @@
 
 #!/bin/bash
 
+DATE=`date "+%H:%M:%S  %d-%m-%Y"`
+
 usage() { echo "Usage: $0 [-i 192.168.0.20] [-n myhost] [-s interval] [-m mail@fqdn.com]" 1>&2; exit 1; }
 
 while getopts ":i:n:s:m:" o; do
@@ -36,21 +38,21 @@ while [ true ]; do
   sleep $s
   fping -r 1 $i > /dev/null
   if [ $? -eq 1 ]; then
-    printf "`date`: $n with IP: $i is DOWN.\n" >> /root/watchman_ping_history.log                                                                           
+    printf "$n / $i is DOWN - $DATE.\n" >> /root/watchman_ping_history.log                                                                           
       if test -f /root/$n"_down"; then
         echo "not sending another mail."
       else
-        echo `date` > mail
-        echo "$i - $n is DOWN!" | mutt -s "date - $i - $n is DOWN!" $m
+        echo $DATE > mail
+        echo "$n / $i is DOWN!" | mutt -s "$n / $i is DOWN! - $DATE" $m
         touch /root/$n"_down"
       fi                                                                                                          
     else
       if test -f /root/$n"_down"; then
         rm /root/$n"_down"                                                                                                                             
-        printf "`date`: $n with IP: $i is UP AGAIN.\n" >> /root/watchman_ping_history.log
-        echo "$i - $n is UP AGAIN!" | mutt -s "date - $i - $n is UP AGAIN!" $m
+        printf "$n / $i is UP AGAIN - $DATE\n" >> /root/watchman_ping_history.log
+        echo "$n / $i is UP AGAIN! - $DATE" | mutt -s "$n / $i is UP AGAIN! - $DATE" $m
       else                                                                                                                              
-      printf "`date`: $n with IP: $i is UP.\n" >> /root/watchman_ping_history.log                                                                             
+      printf "$n / $i is UP - $DATE\n" >> /root/watchman_ping_history.log                                                                             
       fi
   fi
 done
